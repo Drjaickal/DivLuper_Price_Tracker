e7937e68076f93f2883f594b4de0c522401dbd26
Drjaickal <105309360+Drjaickal@users.noreply.github.com>
2024-05-18 14:15:25 +0530

    first commit and fixed the issues of of the import module error

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  experimental: {
+    //serverActions: true,
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  images: {
+    domains: ['m.media-amazon.com']
+  }
+}
+
+ 
+module.exports = nextConfig
